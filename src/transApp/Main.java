4dc96361542984b355f2eca6c818b1a1a1155a28
Jamie <jamielynjenks@gmail.com>
2021-02-09 10:29:01 -0700

    first commit

---
+++ src/transApp/Main.java
@@ -0,0 +1,13 @@
+package transApp;
+
+public class Main 
+{
+    private static void workWithData()
+    {
+        System.out.println("Welcome to Lambda's Animal Kingdom!");
+    }
+    public static void main(String[] args)
+    {
+        workWithData();
+    }
+}
